f26ffe330996c312c0885e0c3492b279e2d22076
tabrown3 <t-a-brown@hotmail.com>
2022-10-17 17:40:43 -0500

    create address counter and tb

---
+++ snes_rom_reader.v
@@ -0,0 +1,28 @@
+module snes_rom_reader
+#(
+    parameter [2:0] FREQ_DIV = 3'd1
+)(
+    input reset,
+    input enable,
+    input clk,
+    input [7:0] data,
+    output reg [19:0] address = 20'h00000
+);
+    // freq_div_cnt is incremented up to FREQ_DIV. When they're equal,
+    //  address is incremented by 1 and freq_div_cnt is reset to 1.
+    reg [2:0] freq_div_cnt = 3'd1;
+
+    always @(negedge clk or posedge reset) begin
+        if (reset) begin
+            address <= 20'h00000;
+            freq_div_cnt <= 3'd1;
+        end else if (enable) begin
+            if (freq_div_cnt < FREQ_DIV) begin
+                freq_div_cnt <= freq_div_cnt + 3'd1;
+            end else begin
+                address <= address + 20'h00001;
+                freq_div_cnt <= 3'd1;
+            end
+        end
+    end
+endmodule
---
+++ tb_snes_rom_reader.v
@@ -0,0 +1,30 @@
+`timescale 10ns/1ns; // slow rom is 200ns
+module tb_snes_rom_reader();
+
+    reg reset = 1'b0;
+    reg enable = 1'b1;
+    reg clk = 1'b1;
+    reg [7:0] data = 8'h00;
+    wire [19:0] address;
+
+    // master clk (100ns) is divided by 2 for slow rom to get 200ns
+    snes_rom_reader #(.FREQ_DIV(3'd2)) DUT(
+        .reset(reset),
+        .enable(enable),
+        .clk(clk),
+        .data(data),
+        .address(address)
+    );
+
+    initial begin
+        #300;
+        $stop;
+    end
+
+    always begin
+        #5;
+        clk <= ~clk;
+        #5;
+        clk <= ~clk;
+    end
+endmodule
